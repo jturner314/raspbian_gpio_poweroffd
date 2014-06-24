3c28b35f63a739cbc3f69836225c59cc655a9d8d
Jim Turner <jturner314@gmail.com>
2014-06-24 02:04:32 -0400

    Add docstrings

--- gpio_poweroffd.py
+++ gpio_poweroffd.py
@@ -12,23 +12,35 @@ syslog_format = 'thinkpad-scripts: %(name)s %(levelname)s %(message)s'
 
 
 def setup_GPIO(channel):
+    """Setup GPIO to read from specified channel.
+
+    :param channel: GPIO channel number (board notation)
+    """
     GPIO.setmode(GPIO.BOARD)
     GPIO.setwarnings(False)
     GPIO.setup(channel, GPIO.IN)
 
 
 def setup_logging():
-    logging.basicConfig(level=logging.INFO, format=console_format)
+    """Setup logging to use system logger.
 
+    :return: this module's Logger instance
+    """
+    logging.basicConfig(level=logging.INFO, format=console_format)
     syslog = logging.handlers.SysLogHandler(address='/dev/log')
     syslog.setLevel(logging.DEBUG)
     syslog.setFormatter(logging.Formatter(syslog_format))
     logging.getLogger().addHandler(syslog)
-
     return logging.getLogger(__name__)
 
 
 def main(channel, hold_time):
+    """Listen for `channel` to go to False. If it remains False for at
+    least `hold_time`, then run ``poweroff``.
+
+    :param channel: GPIO channel number (board notation)
+    :param hold_time: hold time in seconds
+    """
     setup_GPIO(channel)
     logger = setup_logging()
     while True:
